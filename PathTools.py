88bc2737bd6c16ea51a301884aebb92a4998a740
PJ Dietz <pjdietz@gmail.com>
2014-04-02 09:37:24 -0400

    Always return bool from is_enabled() methods to prevent Sublime 3 error.

--- PathTools.py
+++ PathTools.py
@@ -5,78 +5,89 @@ import sublime_plugin
 
 
 class InsertFilePathCommand(sublime_plugin.TextCommand):
+
     def run(self, edit):
         file_path = self.view.file_name()
         replace_text_in_selections(self.view, edit, file_path)
 
     def is_enabled(self):
-        return self.view.file_name() and len(self.view.file_name()) > 0
+        return bool(self.view.file_name() and len(self.view.file_name()) > 0)
+
 
 class InsertRelativePathCommand(sublime_plugin.TextCommand):
+
     def run(self, edit):
         projectFolders = self.view.window().folders()
         self.path = self.view.file_name()
         for folder in projectFolders:
             if folder in self.view.file_name():
                 self.path = self.path.replace(folder, '')
-                break;
+                break
         replace_text_in_selections(self.view, edit, self.path)
 
     def is_enabled(self):
         if self.view.window().folders():
-            return self.view.file_name()
+            return bool(self.view.file_name())
+        return False
 
 
 class InsertFileNameCommand(sublime_plugin.TextCommand):
+
     def run(self, edit):
         file_name = os.path.basename(self.view.file_name())
         replace_text_in_selections(self.view, edit, file_name)
 
     def is_enabled(self):
-        return self.view.file_name() and len(self.view.file_name()) > 0
+        return bool(self.view.file_name() and len(self.view.file_name()) > 0)
 
 
 class InsertFileDirectoryCommand(sublime_plugin.TextCommand):
+
     def run(self, edit):
         file_directory = os.path.dirname(self.view.file_name())
         replace_text_in_selections(self.view, edit, file_directory)
 
     def is_enabled(self):
-        return self.view.file_name() and len(self.view.file_name()) > 0
+        return bool(self.view.file_name() and len(self.view.file_name()) > 0)
+
 
 class InsertRelativeDirectoryCommand(sublime_plugin.TextCommand):
+
     def run(self, edit):
         projectFolders = self.view.window().folders()
         self.directory = os.path.dirname(self.view.file_name())
         for folder in projectFolders:
             if folder in self.view.file_name():
                 self.directory = self.directory.replace(folder, '')
-                break;
+                break
         replace_text_in_selections(self.view, edit, self.directory)
 
     def is_enabled(self):
         if self.view.window().folders():
-            return self.view.file_name()
+            return bool(self.view.file_name())
+        return False
 
 
 class CopyFileNameCommand(sublime_plugin.TextCommand):
+
     def run(self, edit):
         file_name = os.path.basename(self.view.file_name())
         sublime.set_clipboard(file_name)
         sublime.status_message("Copied file name: %s" % file_name)
 
     def is_enabled(self):
-        return self.view.file_name() and len(self.view.file_name()) > 0
+        return bool(self.view.file_name() and len(self.view.file_name()) > 0)
 
 
 class CopyFileDirectoryCommand(sublime_plugin.TextCommand):
+
     def run(self, edit):
         file_directory = os.path.dirname(self.view.file_name())
         sublime.set_clipboard(file_directory)
         sublime.status_message("Copied file directory: %s" % file_directory)
 
     def is_enabled(self):
-        return self.view.file_name() and len(self.view.file_name()) > 0
+        return bool(self.view.file_name() and len(self.view.file_name()) > 0)
 
 
 def replace_text_in_selections(view, edit, text):
