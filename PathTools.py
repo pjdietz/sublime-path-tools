395c8244fd626349ca45529bbe951286f4430cd8
Nils Wiersma <nils.wiersma@gmail.com>
2021-07-20 12:58:43 +0200

    added crude support for line numbers

--- PathTools.py
+++ PathTools.py
@@ -104,6 +104,42 @@ class CopyRelativePathCommand(sublime_plugin.TextCommand):
     def is_enabled(self):
         return bool(self.view.file_name() and len(self.view.file_name()) > 0)
 
+class CopyRelativePathLineCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        projectFolders = self.view.window().folders()
+        self.path = self.view.file_name()
+        for folder in projectFolders:
+            if folder in self.view.file_name():
+                self.path = self.path.replace(folder, '')[1:]
+                break
+
+        out = []
+        for s in self.view.selection:
+            begin = None
+            end = None
+            for l in self.view.lines(s):
+                if not begin:
+                    begin = self.view.rowcol(l.begin())[0]+1
+                else:
+                    begin = min(begin, self.view.rowcol(l.begin())[0]+1)
+
+                if not end:
+                    end = self.view.rowcol(l.end())[0]+1
+                else:
+                    end = max(end, self.view.rowcol(l.end())[0]+1)
+
+            if begin == end:
+                out.append('%s:%d' % (self.path, begin))
+            else:
+                out.append('%s:%d-%d' % (self.path, begin, end))
+
+        sublime.set_clipboard(';'.join(out))
+        sublime.status_message("Copied file directory: %s" % ';'.join(out))
+
+    def is_enabled(self):
+        return bool(self.view.file_name() and len(self.view.file_name()) > 0)
+
 def replace_text_in_selections(view, edit, text):
     """Replace every selection with the passed text"""
     for region in view.sel():
